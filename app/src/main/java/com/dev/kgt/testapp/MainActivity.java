803989058aab14c950fd72e2a9ba2b11b0026615
KyleT082 <kyletruebody@gmail.com>
2016-10-29 13:07:00 +0100

    Initial commit

---
+++ app/src/main/java/com/dev/kgt/testapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dev.kgt.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button myButton = (Button)findViewById(R.id.my_button);
+
+    }
+}
